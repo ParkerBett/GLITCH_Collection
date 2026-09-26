4509f4c38d1674619906eaf69f2b3fff13c5bad6
Parker Bettridge <parkerbett270@gmail.com>
2026-09-26 16:05:29 +1200

    Fully Finished :3

--- scripts/global.gd
+++ scripts/global.gd
@@ -9,11 +9,6 @@ var digital_circus_main_camera = true
 var digital_circus_number = 1
 
 func play_video(video: String):
-	var video_player = load("res://scenes/video_player.tscn")
-	var video_player_instance = video_player.instantiate()
-
-	video_player_instance.video = video
-	
 	var root = get_tree().root
 	var current_scene = get_tree().current_scene
 
@@ -21,6 +16,11 @@ func play_video(video: String):
 
 	root.remove_child.call_deferred(current_scene)
 	current_scene.queue_free()
+	
+	var video_player = load("res://scenes/video_player.tscn")
+	var video_player_instance = video_player.instantiate()
+	
+	video_player_instance.video = video
 	
 	root.add_child.call_deferred(video_player_instance)
 	get_tree().set_current_scene.call_deferred(video_player_instance)
